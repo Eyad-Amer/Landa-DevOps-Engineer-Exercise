980e638fcc5cc7b22b00272407c7b7f83e72fa0e
Eyad-Amer <40535130+Eyad-Amer@users.noreply.github.com>
2023-03-07 12:05:14 +0200

    Update readnugetversion.ps1

--- readnugetversion.ps1
+++ readnugetversion.ps1
@@ -1,13 +1,4 @@
-# install the NuGet package provider
-Install-PackageProvider -Name NuGet -Force
 
-# function to prepend timestamp to output
-function Write-TimestampedOutput($message) {
-    Write-Host ("[{0}] {1}" -f (Get-Date -Format "yyyy-MM-dd HH:mm:ss"), $message)
-}
-
-# Change the working directory to the directory containing the script
-Set-Location $PSScriptRoot
 
 # Define variables
 $packageName = "autoMapper"
@@ -19,24 +10,30 @@ $nuspecFileName = "$packageName.$packageVersion.nuspec"
 $nugetAPIName = "DemoToken"
 $nugetAPIKey = "6vpz5tlz6rv33qetuwpmrpdw5w5t54dpj5lg37s7d26gtpl7lqsq"
 
-# a. Restore the AutoMapper package
+#-------------------------------------------------------------------------
 
-# Check if the package is already installed
-if (Get-Package $packageName -ErrorAction SilentlyContinue) {
-    Write-TimestampedOutput "Package $packageName is already installed."
-    $installedVersion = (Get-Package $packageName).Version
-    Write-TimestampedOutput "Installed version: $installedVersion"
-    # If the installed version is not the required version, uninstall it
-    if ($installedVersion -ne $packageVersion) {
-        Uninstall-Package $packageName -Force
-        Write-TimestampedOutput "Uninstalled package $packageName version $installedVersion."
-    }
+# f. function to prepend timestamp to output
+function Write-TimestampedOutput($message) {
+    Write-Host ("[{0}] {1}" -f (Get-Date -Format "yyyy-MM-dd HH:mm:ss"), $message)
 }
-# Install the package if it's not already installed
-else {
-    Install-Package $packageName -RequiredVersion $packageVersion -Source $nugetUrl
-    Write-TimestampedOutput "Installed package $packageName version $packageVersion."
-}
+
+#-------------------------------------------------------------------------
+
+# a. Restore a NuGet package AutoMapper version 12.0.1 from public repository
+
+# Install the NuGet provider for PackageManagement
+Install-PackageProvider -Name NuGet
+
+# install the package
+nuget.exe install AutoMapper -Version 12.0.1 -Source https://api.nuget.org/v3/index.json
+Write-TimestampedOutput "Installed package $packageName version $packageVersion."
+
+#-------------------------------------------------------------------------
+
+# b. Read the version of the nuspec file of the NuGet package AutoMapper and write it to console.
+
+# first, Install the NuGet.CommandLine package
+Install-Package NuGet.CommandLine
 
 # find the path to the nuspec file of the package
 $nuspecPath = (Get-ChildItem -Path "C:\Users\eyada\.nuget\packages\$packageName\$packageVersion" -Filter *.nuspec).FullName
@@ -44,20 +41,20 @@ $nuspecPath = (Get-ChildItem -Path "C:\Users\eyada\.nuget\packages\$packageName\
 # Open the nuspec file in a text editor
 #notepad $nuspecPath
 
-# b. read the version information from the nuspec file
+# read the version information from the nuspec file
 [xml]$nuspec = Get-Content "C:\Users\eyada\.nuget\packages\automapper\12.0.1\automapper.nuspec"
 $version = $nuspec.package.metadata.version
 Write-TimestampedOutput "The version of the NuGet package is $version"
 
-# c. Install the NuGet.CommandLine package if it is not already installed
-Install-Package NuGet.CommandLine
+#-------------------------------------------------------------------------
+
+# c. Push the NuGet package into the DemoFeed in Azure Artifacts
 
 # if we want to remove azureArtifactFeed
 #nuget.exe sources remove -Name $azureArtifactFeed
 
 # Authenticate to your Azure DevOps organization
 nuget.exe sources Add -Name $azureArtifactFeed -Source "https://pkgs.dev.azure.com/Landa-ExerciseOrg/ExerciseProject/_packaging/DemoFeed/nuget/v3/index.json" -UserName $nugetAPIName -Password $nugetAPIKey
-
 
 # List all packages in the "DemoFeed" NuGet source
 $packages = nuget.exe list -Source "DemoFeed"
@@ -75,13 +72,19 @@ if ($package) {
     nuget.exe push -Source $azureArtifactFeed -ApiKey az automapper.12.0.1.nupkg
 }
 
-# nuget.exe delete AutoMapper 12.0.1 -Source DemoFeed -ApiKey $nugetAPIKey
+#-------------------------------------------------------------------------
 
 # d. to Change the version of the nuspec file of the NuGet package AutoMapper from 12.0.1 to 14.1.3
+
 # Change the version number in the nuspec file
 (Get-Content $nuspecPath) | ForEach-Object { $_ -replace '12.0.1', '14.1.3' } | Set-Content $nuspecPath
 
+#-------------------------------------------------------------------------
+
 # e. Read the nuspec file again and write to console the (new) version from the file.
+
 [xml]$nuspec = Get-Content "C:\Users\eyada\.nuget\packages\automapper\12.0.1\automapper.nuspec"
 $version = $nuspec.package.metadata.version
 Write-TimestampedOutput "The version of the NuGet package is $version"
+
+#-------------------------------------------------------------------------
